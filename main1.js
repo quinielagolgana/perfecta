dddf4b90023a9e638c19fa4f9691172668531d62
quinielagolgana <68973745+quinielagolgana@users.noreply.github.com>
2021-02-25 03:26:08 -0600

    Add files via upload

--- main1.js
+++ main1.js
@@ -277,15 +277,3 @@ function removeLastLine(){
 }
 
 window.addEventListener("load",start,false);
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
